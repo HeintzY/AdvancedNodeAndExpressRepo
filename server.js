3ab4f920d3b67ac17b562617714e6e5ab1c87103
HeintzY <heintzymail@testmail.com>
2025-05-14 11:16:22 +0200

    feat: add How to Put a Profile Together

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ myDB(async client => {
   });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render('profile');
+    res.render('profile', { username: req.user.username });
   });
 
   // Serialization and deserialization here...

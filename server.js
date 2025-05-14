e8e0eb25ae177e8ef25933e817fb7548679cfe05
HeintzY <heintzymail@testmail.com>
2025-05-14 11:22:10 +0200

    feat: add Logging a User Out

--- server.js
+++ server.js
@@ -46,6 +46,18 @@ myDB(async client => {
     res.render('profile', { username: req.user.username });
   });
 
+  app.route('/logout')
+    .get((req, res) => {
+      req.logout();
+      res.redirect('/');
+    });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
   // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
     done(null, user._id);
@@ -62,6 +74,7 @@ myDB(async client => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
+
 
 passport.use(new LocalStrategy((username, password, done) => {
   myDataBase.findOne({ username: username }, (err, user) => {

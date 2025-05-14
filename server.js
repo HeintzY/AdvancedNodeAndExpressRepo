810001ae2ed91961df712784a92002f9b4304cef
HeintzY <heintzymail@testmail.com>
2025-05-14 11:40:13 +0200

    feat: add Hashing Your Passwords

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 const session = require("express-session");
 const passport = require('passport');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -55,6 +58,7 @@ myDB(async client => {
 
   app.route('/register')
     .post((req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12);
       myDataBase.findOne({ username: req.body.username }, (err, user) => {
         if (err) {
           next(err);
@@ -63,7 +67,7 @@ myDB(async client => {
         } else {
           myDataBase.insertOne({
             username: req.body.username,
-            password: req.body.password
+            password: hash
           },
             (err, doc) => {
               if (err) {
@@ -113,7 +117,9 @@ passport.use(new LocalStrategy((username, password, done) => {
     console.log(`User ${username} attempted to log in.`);
     if (err) return done(err);
     if (!user) return done(null, false);
-    if (password !== user.password) return done(null, false);
+    if (!bcrypt.compareSync(password, user.password)) {
+      return done(null, false);
+    }
     return done(null, user);
   });
 }));
